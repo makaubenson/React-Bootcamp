7bfab84a8b49c69a3a272fdf30cc67cc8321492a
benson <bensonmakau2000@gmail.com>
2023-01-29 13:21:32 +0300

    splitting components into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+
 //A component in react is just a js function.
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-Us", { month: "long" });
-  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
-  const year = props.date.getFullYear();
   //we access the set attributes using props.(which stands for properties)
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
