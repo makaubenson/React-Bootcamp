e2c74f2ad0130dc301f9d0c78834a33182604d81
makaubenson <bensonmakau2000@gmail.com>
2023-05-16 16:25:38 +0300

    deleted all files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import "./index.css";
-
-//import App.js
-import App from "./App"; //Component
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />); //JSX Syntax
