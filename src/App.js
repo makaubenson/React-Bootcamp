fa4d236c25313203b826b25b7595111adda36dea
Benson <bensonmakau2000@gmail.com>
2023-07-05 12:01:26 +0300

    adding form inputs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import NewExpense from './components/NewExpense/NewExpense'
 import Expenses from "./components/Expenses/Expenses";
 
 //root component
@@ -26,6 +29,7 @@ const App = () =>{
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
