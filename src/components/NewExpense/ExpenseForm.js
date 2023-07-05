92d5aaccc29fce42debcadfe582207f0bc3f9cba
Benson <bensonmakau2000@gmail.com>
2023-07-05 12:29:22 +0300

    working with multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
 import './ExpenseForm.css'; 
 
 const ExpenseForm = () => {
-    const titleChangeHandler = (event) => {
+   const [enteredTitle, setEnteredTitle]= useState('');
 
-        console.log(event.target.value);
+   const [enteredAmount, setEnteredAmount]= useState('');
+
+   const [enteredDate, setEnteredDate]= useState('');
+   
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        // console.log(event.target.value);
     }
 
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        // console.log(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // console.log(event.target.value);
+    }
     return (
         <form>
             <div className="new-expense__controls">
@@ -16,11 +31,11 @@ const ExpenseForm = () => {
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01"  step="0.01"/>
+                    <input type="number" min="0.01"  step="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31"/>
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
