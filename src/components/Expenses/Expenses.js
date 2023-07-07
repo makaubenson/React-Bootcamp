79a38158f6308a6465c185ab0ba543c39f170a90
Benson <bensonmakau2000@gmail.com>
2023-07-07 10:10:49 +0300

    using stateful lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,29 +1,34 @@
-import React,{ useState } from "react";
-import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpensesFilter";
-import Card from "../UI/Card";
-import "./Expenses.css";
+import React, { useState } from 'react';
 
-  const Expenses = (props) => {
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
-  
-  const filterChangeHandler = selectedYear => {
-      setFilteredYear(selectedYear);
-    }
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-     {props.items.map((expense) => (
-     <ExpenseItem
-     title={expense.title} 
-     amount={expense.amount} 
-     date={expense.date}
-     />
-     ))}
-  
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
